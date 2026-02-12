def98ee939c51d6808fe0eb8fd37a8184f2ba97d
sarveshpatil <sarveshwarpatil96@gmail.com>
2026-02-12 15:09:30 +0530

    sma-5 getStudentApi

--- StudentApplication/src/main/java/com/example/demo/StudentController.java
+++ StudentApplication/src/main/java/com/example/demo/StudentController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,13 @@ public class StudentController {
 	
 	@PostMapping("/add")
 	public ResponseEntity<Student> addStudent(@RequestBody Student student)
+	{
+		return null;
+		
+	}
+	
+	@GetMapping("/get")
+	public ResponseEntity<Student> getStudent(@PathVariable int id)
 	{
 		return null;
 		
